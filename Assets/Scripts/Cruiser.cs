d1c32a19ecdcf28b01d801d0613cde46143de85e
suhang <suhang@roobo.com>
2021-11-04 18:03:51 +0800

    floating things

--- Assets/Scripts/Cruiser.cs
+++ Assets/Scripts/Cruiser.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
-
+using DG.Tweening;
 
 public class Cruiser : MonoBehaviour
 {
-
+    [Required]
+    public IScrollLimiter scrollLimiter;
+    private float end;
+    private float start;
     // Start is called before the first frame update
     void Start()
-    {       
+    {
+        //正确的值应该是screen width ➗2 但是这里不➗2是为了让飞行物飞出屏幕
+         end = GetComponentInParent<PositionBridge>().getRight()+ScreenSize.getScreenWidth();
+        start = -ScreenSize.getScreenWidth() / 2;
+        Debug.Log("limit:"+end);
+        transform.DOMoveX(end,5);
     }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/ScreenSize.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize
+{
+    public static float getScreenWidth()
+    {
+        return 22f;
+    }
+    public static float getScreenHeight()
+    {
+        return 5.7f;
+    }
+}
