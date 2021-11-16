6efd9755015a6e56f7c3c1801e01bb7d8e7cc1ae
Tyrant <>
2021-11-16 09:49:53 +0800

    增加shapes

--- Assets/Scripts/Draw/Line/DrawLine.cs
+++ Assets/Scripts/Draw/Line/DrawLine.cs
@@ -2,26 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Shapes;
+
 public class DrawLine : MonoBehaviour
 {
 
+    public Line line;
 
-    public LineRenderer line;
-
-
-    public void DrawTheLine(Vector3 from, Vector3 to)
+    private void Awake()
     {
-        line.positionCount = 2;
+        line = GetComponent<Line>();
+    }
 
-        line.SetPosition(0, from);
+    public void DrawTheLine(Transform from, Transform to)
+    {
+        //line.positionCount = 2;
+        //line.Start = from;
 
-        line.SetPosition(1, to);
+        var p = transform.position;
+        line.End = transform.InverseTransformPoint(to.position);
+        //line.End = new Vector3(to.position.x, -(p - to.position).magnitude, 0);
+        Logging.Log(transform.localPosition);
+        Logging.Log(p);
+        Logging.Log(to.position);
+        Logging.Log((p - new Vector3(0, to.position.y, 0)).magnitude);
+        Logging.Log((p - to.position).magnitude);
+        //line.SetPosition(0, from);
+
+
+        Logging.Log(transform.InverseTransformPoint(to.position));
+
+        //line.SetPosition(1, to);
     }
 
 
     public void ClearLine()
     {
-        line.positionCount = 0;
+        line.Start = Vector3.zero;
+        line.End = Vector3.zero;
+        //line.positionCount = 0;
     }
 
 }
