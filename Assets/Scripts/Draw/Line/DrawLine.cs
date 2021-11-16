6632134d4ae59adaf9a775df801d1b0c8dd962c6
Tyrant <>
2021-11-16 11:11:22 +0800

    更新

--- Assets/Scripts/Draw/Line/DrawLine.cs
+++ Assets/Scripts/Draw/Line/DrawLine.cs
@@ -16,23 +16,7 @@ public class DrawLine : MonoBehaviour
 
     public void DrawTheLine(Transform from, Transform to)
     {
-        //line.positionCount = 2;
-        //line.Start = from;
-
-        var p = transform.position;
         line.End = transform.InverseTransformPoint(to.position);
-        //line.End = new Vector3(to.position.x, -(p - to.position).magnitude, 0);
-        Logging.Log(transform.localPosition);
-        Logging.Log(p);
-        Logging.Log(to.position);
-        Logging.Log((p - new Vector3(0, to.position.y, 0)).magnitude);
-        Logging.Log((p - to.position).magnitude);
-        //line.SetPosition(0, from);
-
-
-        Logging.Log(transform.InverseTransformPoint(to.position));
-
-        //line.SetPosition(1, to);
     }
 
 
