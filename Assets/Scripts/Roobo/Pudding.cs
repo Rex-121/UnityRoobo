fb9c2cf2be3ce5311e6fcdf8b4b15dfc20e71bd5
ZT <zt7623694@sina.com>
2021-12-08 17:34:09 +0800

    no message

--- Assets/Scripts/Roobo/Pudding.cs
+++ Assets/Scripts/Roobo/Pudding.cs
@@ -2,22 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Spine.Unity;
+using UniRx;
 
 public enum PuddingAction
 {
-    hello, speak, unhappy, bubble, starHappy, starMagic, starSpeak
+    idle, hello, speak, unhappy, bubble, starHappy, starMagic, starSpeak
 }
 
 
 public class Pudding : MonoBehaviour
 {
+
+    ContentPlayer player;
     Animator animator;
 
+    public ReactiveProperty<PlayerEvent> speakStatus
+    {
+        get { return player.status; }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GetComponent<ContentPlayer>();
         animator = GetComponent<Animator>();
+
+        player.status.Subscribe(status =>
+        {
+            switch (status)
+            {
+
+            }
+            speakStatus.Value = status;
+        });
     }
 
     // Update is called once per frame
@@ -31,6 +48,11 @@ public class Pudding : MonoBehaviour
         animator.SetTrigger(action.ToString());
 
     }
- 
 
+    public void Speak(string content, string type)
+    {
+        player.PlayContentByType(content, type);
+    }
+ 
+    
 }
